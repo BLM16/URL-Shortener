6827c96977076f21978d1286a2f2f1cefc623fe1
Bradley Myers <myers.bradley@hotmail.com>
2021-01-27 16:56:07 -0500

    Added basic functionality and error handling

--- Run.py
+++ Run.py
@@ -1,8 +1,14 @@
+from flask import redirect, url_for
+
 from config import app
 from routes import routes
 
 # Register the app's routes
 app.register_blueprint(routes, url_prefix = "")
+
+@app.errorhandler(404)
+def PageNotFound(e):
+    return redirect(url_for('routes.Error', title = "Error: 404 - page not found", msg = e))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- routes.py
+++ routes.py
@@ -6,6 +6,30 @@ from config import engine
 
 routes = Blueprint("routes", __name__, static_folder = 'static', template_folder = 'templates')
 
+@routes.errorhandler(404)
+def PageNotFound(e):
+    return redirect(url_for('routes.Error', title = "Error: 404 - page not found", msg = e))
+
 @routes.route('/')
 def Index():
     return render_template("index.html")
+
+@routes.route('/<int:key>')
+def KeyRedir(key):
+    # Connect to the DB
+    with engine.connect() as con:
+        # Get the url associated with the key
+        sql = text("SELECT url FROM url WHERE key = :key")
+        url = con.execute(sql, key = key).scalar()
+
+    # Redirect to the url for the key
+    return redirect(url)
+
+@routes.route('/error')
+def Error():
+    # Get the error parameters
+    title = request.args.get('title')
+    msg = request.args.get('msg')
+    back = request.args.get('back')
+
+    return render_template("error.html", title = title, msg = msg, back = back)
